a17d7a77ca745f951d59e210ce3c6aa720b8fac6
seus31 <us.balut.big.dipper@gmail.com>
2025-02-08 07:42:19 +0900

    ダッシュボードページの作成

--- app/src/pages/Dashboard.tsx
+++ app/src/pages/Dashboard.tsx
@@ -1,8 +1,13 @@
-import './Dashboard.css'
+import UserLayout from '../layouts/UserLayout.tsx'
 
 const Dashboard = () => {
     return (
         <>
+            <UserLayout>
+                <div className="p-4">
+                    <h1 className="text-2xl font-bold">Dashboard</h1>
+                </div>
+            </UserLayout>
         </>
     )
 }
