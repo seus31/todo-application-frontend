1e075f4cd39fba00b8ab3b8355383007eeebad77
seus31 <us.balut.big.dipper@gmail.com>
2025-02-07 15:31:42 +0900

    各種ルートの設定

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import ProtectedRoute from './components/ProtectedRoute.tsx'
+import RedirectIfAuthenticated from './components/RedirectIfAuthenticated.tsx'
 import AdminLogin from './pages/admin/AdminLogin.tsx'
+import Dashboard from './pages/Dashboard.tsx'
 import Login from './pages/Login.tsx'
+import NotFoundPage from './pages/NotFoundPage.tsx'
+import Register from './pages/Register.tsx'
 import './App.css'
 
 const App = () => {
@@ -8,7 +13,22 @@ const App = () => {
         <BrowserRouter>
             <Routes>
                 <Route path="/admin/login" element={<AdminLogin />} />
-                <Route path="/login" element={<Login />} />
+                <Route path="/login" element={
+                    <RedirectIfAuthenticated>
+                        <Login />
+                    </RedirectIfAuthenticated>
+                } />
+                <Route path="/register" element={
+                    <RedirectIfAuthenticated>
+                        <Register />
+                    </RedirectIfAuthenticated>
+                } />
+                <Route path="/" element={
+                    <ProtectedRoute>
+                        <Dashboard />
+                    </ProtectedRoute>
+                } />
+                <Route path="*" element={<NotFoundPage />} />
             </Routes>
         </BrowserRouter>
     );
