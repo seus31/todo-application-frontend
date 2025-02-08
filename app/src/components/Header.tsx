b773b8ebcf3ad881706b612fb03b961cd5fff711
seus31 <us.balut.big.dipper@gmail.com>
2025-02-08 13:02:58 +0900

    ログアウト処理の実装

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -1,11 +1,21 @@
+import { useNavigate } from 'react-router-dom'
+
 const Header = () => {
+    const navigate = useNavigate()
+
+    const handleLogout = () => {
+        localStorage.removeItem('token')
+        navigate('/login')
+    }
+
     return (
         <>
             <div className="flex items-center justify-between h-16 bg-white border-b border-gray-200">
                 <div className="flex items-center px-4"></div>
                 <div className="flex items-center pr-4">
                     <button
-                        className="mr-3 flex items-center text-gray-500 hover:text-gray-700 focus:outline-none focus:text-gray-700">
+                        className="mr-3 flex items-center text-gray-500 hover:text-gray-700 focus:outline-none focus:text-gray-700"
+                        onClick={handleLogout}>
                         ログアウト
                     </button>
                 </div>
